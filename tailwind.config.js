5038b7cd60491fa965f63633f16719a80d997c4d
Shadman <shadman.alizada@gmail.com>
2024-03-21 23:09:04 +0400

    feat: added fonts, colors and breakpoints in tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1264px",
+      },
+    },
+
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        avantGarde: ["AvantGardeBold", "sans-serif"],
+        tradegothic: ["tradegothic", "sans-serif"],
+        "tradegothic-light": ["tradegothic-light", "sans-serif"],
+      },
+      colors: {
+        "primary-clr": "#e9204f",
+        "secondary-clr": "#1b1b1b",
+        "dark-clr": "#101010",
+        "gray-clr": "#747474",
+        "gray-light": "#9c9c9c",
+        "t-hover": "#ffb2c4",
+      },
+    },
+  },
+  plugins: [],
+};
